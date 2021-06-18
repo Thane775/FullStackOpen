7c7209f673301b23913a350850587e89df936362
Thane775 <85929118+Thane775@users.noreply.github.com>
2021-06-18 18:47:00 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Course from './C'
+import Course from './Components'
 
 const App = () => {
     /* exercises 2.1 to 2.3 */
@@ -89,72 +89,6 @@ const App = () => {
             {kuursit}
         </div>
     )
-}
-
-export default App
- 
-
-  return (
-      <div>
-      <Header />
-          <p>{parts}</p>
-          {parts.map(parts => <div> {parts.name} {parts.exercises}</div>)}
-       </div>
-  )
-  
-  
-    
-}*/
-
-
-const Header = (props) => {
- // const course = 'Half Stack application development'
-  return(
-   <div>
-    <h1>{props.course}</h1>
-   </div> 
-  )
-}
-
-const Content = (props) => {
- // const part1 = 'Fundamentals of React'
-  //const part2 = 'Using props to pass data'
-  //const part3 = 'State of a component'
-
-return(
-  <div>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
-  </div>
-
-  )
-}
-
-const Total = () =>{
-  const tootal = 10 + 7 + 14
-  return(
-    <p>Number of exercise {tootal}</p>
-  )
-}
-
-const App = () => {
-  
-  
-
-  return (
-    <div>
-      <Header course="Fundamentals of React"/>
-      <Content part1="Fundamentals of React" exercises1="10" part2="Using props to pass data" exercises2="7" part3="State of a component" exercises3="14" />
-      <Total />
-    </div>
-  )
 }
 
 export default App
